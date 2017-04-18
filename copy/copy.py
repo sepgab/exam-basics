6ae2ac65715ab88a7e5d2808038f7ef3d1dc7df0
Sepler Gábor <sepler.gabor@gmail.com>
2017-04-18 09:49:38 +0200

    Copy function feat(usage print)

--- copy/copy.py
+++ copy/copy.py
@@ -6,3 +6,26 @@
 # No destination provided
 # When both arguments provided and the source is a file
 # Read all contents from it and write it to the destination
+
+import sys
+
+class Controller():
+
+    def __init__(self):
+        self.list_argv = []
+        self.arg_reader()
+        self.manager()
+
+    def arg_reader(self):
+        if len(sys.argv) <= 1:
+            self.list_argv = []
+        else:
+            self.list_argv = sys.argv[1:]
+
+    def manager(self):
+        if len(self.list_argv) == 0:
+            print('copy [source] [destination]')
+        else:
+            pass
+
+controller = Controller()
