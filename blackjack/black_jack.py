2b1223eecb9152547b4bd3cee7b1658d5ac68b98
Sepler Gábor <sepler.gabor@gmail.com>
2017-04-18 11:45:26 +0200

    Black Jack feat(Deck representation)

--- blackjack/black_jack.py
+++ blackjack/black_jack.py
@@ -23,29 +23,49 @@
 
 import random
 
-list_of_colors = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
-list_of_values = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+
 
 class Card():
 
     def __init__(self):
-        self.color = random.choice(list_of_colors)
-        self.value = random.choice(list_of_values)
+        self.list_of_colors = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
+        self.list_of_values = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+        self.color = random.choice(self.list_of_colors)
+        self.value = random.choice(self.list_of_values)
         self.card = str(self.value) + ' ' + str(self.color)
-        return self.card
 
 class Deck():
 
     def __init__(self, number_of_cards):
         self.number_of_cards = int(number_of_cards)
-        self.card_to_add = None
+        self.number_of_clubs = 0
+        self.number_of_diamonds = 0
+        self.number_of_hearts = 0
+        self.number_of_spades = 0
+        self.to_add = None
         self.list_of_cards = []
         self.counter_cards = 0
+        self.upload()
+
+    def upload(self):
         while self.counter_cards < self.number_of_cards:
-            self.card_to_add = Card()
-            if self.card_to_add not in self.list_of_cards:
-                self.list_of_cards.append(card_to_add)
+            self.to_add = Card()
+            if self.to_add.card not in self.list_of_cards:
+                self.list_of_cards.append(self.to_add.card)
                 self.counter_cards += 1
+                if self.to_add.color == 'Clubs':
+                    self.number_of_clubs += 1
+                elif self.to_add.color == 'Diamonds':
+                    self.number_of_diamonds += 1
+                elif self.to_add.color == 'Hearts':
+                    self.number_of_hearts += 1
+                else:
+                    self.number_of_spades += 1
+
+    def __str__(self):
+        result = ""
+        result = str(self.number_of_cards) + ' cards -  ' + str(self.number_of_clubs) + ' Clubs, ' + str(self.number_of_diamonds) + ' Diamonds, ' + str(self.number_of_hearts) + ' Hearts, ' + str(self.number_of_spades) + ' Spades'
+        return result
 
 deck = Deck(12)
 
