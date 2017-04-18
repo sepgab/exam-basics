9c87820c8549b35d950f73b36fb9476d691d4b1c
Sepler Gábor <sepler.gabor@gmail.com>
2017-04-18 09:29:19 +0200

    Odd_average function created

--- oddavg/odd_avg.py
+++ oddavg/odd_avg.py
@@ -1,3 +1,16 @@
 # Create a function called `odd_average` that takes a list of numbers as parameter
 # and returns the average value of the odd numbers in the list
 # Create basic unit tests for it with at least 3 different test cases
+
+def odd_average(list_of_numbers):
+    average_odd = 'No odd numbers in the list'
+    sum_odd = 0
+    counter_odd = 0
+    for number in list_of_numbers:
+        if number % 2 == 1:
+            sum_odd += number
+            counter_odd += 1
+    if counter_odd > 0:
+        average_odd = sum_odd / counter_odd
+    return average_odd
+    
